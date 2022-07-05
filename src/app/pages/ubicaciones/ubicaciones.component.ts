ca4e9a08339eace80c4a4fd7b20a1d0c0477f352
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-07-05 13:40:08 -0500

    added ubicacion.model

--- src/app/pages/ubicaciones/ubicaciones.component.ts
+++ src/app/pages/ubicaciones/ubicaciones.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { UbicacionModel } from 'src/app/models/ubicacion.model';
+
+import { UbicacionesService } from 'src/app/services/ubicaciones.service';
 
 @Component({
   selector: 'app-ubicaciones',
@@ -6,10 +11,127 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ubicaciones.component.css']
 })
 export class UbicacionesComponent implements OnInit {
+  statusPantalla: number;
+  listUbicaciones: UbicacionModel[] = [];
 
-  constructor() { }
+  productDialog: boolean;
 
-  ngOnInit(): void {
+  products: any ;
+
+  product: any;
+
+  selectedProducts: any;
+
+  submitted: boolean;
+
+  statuses: any[];
+
+
+  constructor( 
+    private messageService: MessageService, 
+    private confirmationService: ConfirmationService,
+    private ubicacionesService: UbicacionesService) { }
+
+  ngOnInit() {
+     // this.ubicacionesService.getUbicaciones().then(data => this.listUbicaciones = data);
+    this.getUbicaciones();
+
+      this.statuses = [
+          {label: 'INSTOCK', value: 'instock'},
+          {label: 'LOWSTOCK', value: 'lowstock'},
+          {label: 'OUTOFSTOCK', value: 'outofstock'}
+      ];
+  }
+  getUbicaciones(){
+    this.ubicacionesService.getUbicaciones().subscribe((response) => {
+      if(response.exito){
+          this.listUbicaciones = response.respuesta;
+      }
+    });
   }
 
+
+  openNew() {
+      this.product = {};
+      this.submitted = false;
+      this.productDialog = true;
+  }
+
+  deleteSelectedProducts() {
+      this.confirmationService.confirm({
+          message: 'Are you sure you want to delete the selected products?',
+          header: 'Confirm',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+              this.products = this.products.filter(val => !this.selectedProducts.includes(val));
+              this.selectedProducts = null;
+              this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
+          }
+      });
+  }
+
+  editProduct() {
+      this.product = {};
+      this.productDialog = true;
+  }
+
+  deleteProduct(product: any) {
+      this.confirmationService.confirm({
+          message: 'Are you sure you want to delete ' + product.name + '?',
+          header: 'Confirm',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+              this.products = this.products.filter(val => val.id !== product.id);
+              this.product = {};
+              this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
+          }
+      });
+  }
+
+  hideDialog() {
+      this.productDialog = false;
+      this.submitted = false;
+  }
+
+  saveProduct() {
+      this.submitted = true;
+
+      if (this.product.name.trim()) {
+          if (this.product.id) {
+              this.products[this.findIndexById(this.product.id)] = this.product;
+              this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+          }
+          else {
+              this.product.id = this.createId();
+              this.product.image = 'product-placeholder.svg';
+              this.products.push(this.product);
+              this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+          }
+
+          this.products = [...this.products];
+          this.productDialog = false;
+          this.product = {};
+      }
+  }
+
+  findIndexById(id: string): number {
+      let index = -1;
+      for (let i = 0; i < this.products.length; i++) {
+          if (this.products[i].id === id) {
+              index = i;
+              break;
+          }
+      }
+
+      return index;
+  }
+
+  createId(): string {
+      let id = '';
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for ( var i = 0; i < 5; i++ ) {
+          id += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return id;
+  }
 }
