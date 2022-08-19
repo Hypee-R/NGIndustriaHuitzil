5b8adbd6cb23cc3ff1b930fa3045e92fbeba12b9
EdgardoHdez96 <105742688+EdgardoHdez96@users.noreply.github.com>
2022-08-19 18:19:15 -0500

    MOdulo ventas

--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -1,15 +1,90 @@
 import { Component, OnInit } from '@angular/core';
+import { UbicacionModel } from 'src/app/models/ubicacion.model';
+import { VariablesService } from 'src/app/services/variablesGL.service';
+
+
+
+
+class ventaModel{
+  idProducto: string;
+  cantidad:number;
+  caja:string;
+  totalParcial:number;
+  constructor(){
+      this.idProducto = "";
+      this.cantidad=0;
+      this.caja=""
+      this.totalParcial=0
+
+    }
+}
 
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
   styleUrls: ['./ventas.component.css']
 })
-export class VentasComponent implements OnInit {
 
-  constructor() { }
+
+
+
+export class VentasComponent implements OnInit {
+  statusPanubicacion: number;
+  loading: boolean = false;
+  listVentas: ventaModel[] = [];
+  cols: any[] = [];
+  rows = 0;
+  value18=21
+  value8: any;
+  cities: any[];
+  constructor(
+    public variablesGL: VariablesService,
+  ) { 
+
+    this.cols = [
+      { field: 'idProducto', header: 'Producto' },
+      { field: 'cantidad',header:'cantidad'},
+      { field: 'totalParcial', header: 'totalParcial' },
+      { field: 'caja', header: 'caja' },
+      //{ field: 'apellidoPEncargado', header: 'apellidoPEncargado' },
+      //{ field: 'apellidoMEncargado', header: 'apellidoMEncargado' },
+      //{ field: 'telefono1', header: 'telefono1' },
+     // { field: 'telefono2', header: 'telefono2' },
+      //{ field: 'correo', header: 'correo' },
+    ];
+
+    this.cities = [
+      { name: "New York", code: "NY" },
+      { name: "Rome", code: "RM" },
+      { name: "London", code: "LDN" },
+      { name: "Istanbul", code: "IST" },
+      { name: "Paris", code: "PRS" }
+    ];
+
+    this.statusPanubicacion = this.variablesGL.getStatusPantalla();
+      let status = this.variablesGL.getPantalla();
+      if(status == 'celular'){
+        this.rows = 6;
+      }else if(status == 'tablet'){
+        this.rows = 7;
+      }else if(status == 'laptop'){
+        this.rows = 5;
+      }else{
+        this.rows = 11;
+      }
+
+  }
 
   ngOnInit(): void {
+    this.loading=false
+    let venta:ventaModel={idProducto:"Demo1 (Talla 25 ) \n15226 DEMO1",totalParcial:100,caja:"caja1",cantidad:100}
+    this.listVentas.push(venta)
+    this.listVentas.push(venta)
+
+    this.listVentas.push(venta)
+
+
+    //this.listVentas=[venta,venta]
   }
 
 }
