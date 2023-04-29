1902404ddf7a4d09d8970295cbc0119146ebc777
Jose daniel <jose.dejesus@it-and-apps.mx>
2023-04-28 18:21:10 -0600

    minor fix:searchProduct

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  //apiService: "https://localhost:7105/",
+  apiService: "https://localhost:7105/",
   //apiService: "https://localhost:7105/",
   //apiService: "http://huitzil-001-site1.ftempurl.com/",
    // apiService: "https://huitzil-servicesapi.herokuapp.com/",
-  apiService: "https://huitzildev.azurewebsites.net/"
+ // apiService: "https://huitzildev.azurewebsites.net/"
 };
