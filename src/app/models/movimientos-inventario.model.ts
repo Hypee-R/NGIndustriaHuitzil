e54b7b7f928d2b92a415e371e330816b59a18d77
Jose daniel <jose.dejesus@it-and-apps.mx>
2023-06-07 18:51:28 -0600

    minor fix

--- src/app/models/movimientos-inventario.model.ts
+++ src/app/models/movimientos-inventario.model.ts
@@ -12,6 +12,7 @@ export class MovimientosInventarioModel {
     direccion?: string;
     usuarioRecibe?: string;
     usuarioEnvia?: string;
+    tipoPaquete?:string;
     ubicacionDestinodesc:string;
     movimientoArticulos?: MovimientoArticuloModel[];
 }
