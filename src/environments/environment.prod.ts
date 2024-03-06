7f9417f786a7e63fc13c211c751bbd50bf5fdbf2
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2024-03-06 14:41:29 -0600

    configuracion Server Huitzil local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiService: "https://apihuitzil-93cf99d13c22.herokuapp.com/",
+  apiService: "http://localhost:8080/",
  
   //apiService: "http://huitzil-001-site1.ftempurl.com/",
 //apiService: "https://huitzil-servicesapi.herokuapp.com/",
