c91a76a48f05fe2d4d1e924dfa0906c83b9e5720
Jose daniel <jose.dejesus@it-and-apps.mx>
2024-03-19 18:32:15 -0600

    ajustes envirotment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiService: "http://localhost:8080/",
- 
+  apiService: "http://9.tcp.ngrok.io:24529/",
+
   //apiService: "http://huitzil-001-site1.ftempurl.com/",
 //apiService: "https://huitzil-servicesapi.herokuapp.com/",
    //apiService: "https://huitzildev.azurewebsites.net/"
