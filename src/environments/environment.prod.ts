d3fd681d4203a34b7eedc2dbe31589eb3aac5573
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2024-05-13 17:24:10 -0600

    entorno servidor fisico

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiService: "http://localhost:5000/",
+  apiService: "https://pos-api-uniformeshuitzil.infrati-gcs.cloud:9110/",
   //apiService: "http://huitzil-001-site1.ftempurl.com/",
   //apiService: "https://huitzil-servicesapi.herokuapp.com/",
   //apiService: "https://huitzildev.azurewebsites.net/"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
- apiService: "http://localhost:5000/",
+  apiService: "https://pos-api-uniformeshuitzil.infrati-gcs.cloud:9110/",
   // apiService: "http://huitzil-001-site1.ftempurl.com/",
   //apiService: "https://huitzil-servicesapi.herokuapp.com/",
    // apiService: "https://huitzildev.azurewebsites.net/"
