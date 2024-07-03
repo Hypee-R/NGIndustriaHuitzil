817bc0d2c28518ee3e656581b6693030e9dc20f0
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2024-07-02 20:09:31 -0600

    se ajusto el servicio de imprimir etiquetas por localhost

--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -17,7 +17,7 @@ export class InventarioService {
     private variablesGL: VariablesService) { }
 
     getImprimirEtiquetas(Descripcion: string,sku: string,cantidad: number,pc: string): Observable<ResponseModelImprime>{
-      return this.http.get<ResponseModelImprime>(`http://127.0.0.1:5000/imprime?sku=${sku}&descripcion1=${Descripcion.substring(0,20)}&descripcion2=${Descripcion.substring(20,Descripcion.length)}&cantidad=${cantidad}&pcname=${pc}&printer=ZDesigner%20TLP%202844`).pipe(
+      return this.http.get<ResponseModelImprime>(`http://localhost:5000/imprime?sku=${sku}&descripcion1=${Descripcion.substring(0,20)}&descripcion2=${Descripcion.substring(20,Descripcion.length)}&cantidad=${cantidad}&pcname=${pc}&printer=ZDesigner%20TLP%202844`).pipe(
         map (res => res)
       );
     }
