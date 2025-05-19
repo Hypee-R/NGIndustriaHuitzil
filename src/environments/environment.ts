5a0cf5e6ca95ffdd60bba1b84b6417e813bb4696
JDDP <dejesusperezjosedaniel@gmail.com>
2025-05-19 13:17:23 -0600

    add envirotment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiService: "https://pos-api.uniformeshuitzil.com:9110/",
-  apiService: "https://jussaseguridadindustrial.kodeinnventors.com:4436/",
-  //apiService: "http://localhost:5000/",
-  //apiService: "https://huitzil-servicesapi.herokuapp.com/",
-   // apiService: "https://huitzildev.azurewebsites.net/"
+  //apiService: "https://jussaseguridadindustrial.kodeinnventors.com:4436/",
+  apiService: "http://localhost:5000/",
 };
 
 /*
