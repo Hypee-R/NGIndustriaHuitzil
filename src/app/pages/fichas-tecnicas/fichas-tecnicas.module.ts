cd517464232c5b548c2f7eb966ec474db264afe9
JDDP <dejesusperezjosedaniel@gmail.com>
2025-06-26 17:18:55 -0600

    add fichas de produccion

---
+++ src/app/pages/fichas-tecnicas/fichas-tecnicas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FichasTecnicasRoutingModule } from './fichas-tecnicas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FichasTecnicasRoutingModule
+  ]
+})
+export class FichasTecnicasModule { }
